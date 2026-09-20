8ce7e35b6c09b213432ba9a282ed51d2e10f92b7
Raghav267 <harshraghav089@gmail.com>
2026-09-20 10:09:31 +0530

    C++ Basics , Lopps , Pointers

---
+++ 17-sept.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    //     cout << "Size of int: " << sizeof(int) << " bytes" << endl;
+    //     cout << "Size of float: " << sizeof(float) << " bytes" << endl;
+    //     cout << "Size of double: " << sizeof(double) << " bytes" << endl;
+    // //     Size of int: 4 bytes
+    // // Size of float: 4 bytes
+    // // Size of double: 8 bytes
+
+    // int a, b;
+    // cout << "Enter two numbers: ";
+    // cin >> a >> b;
+    // cout << "The two numbers are: " << a << " and " << b << endl;
+
+    // int i = 0;
+    // for (; i < 10;)
+    // {
+    //     i = i + 2;
+    //     cout << "Harsh Raghav" << endl;
+    // }
+
+    // int n;
+    // cin >> n;
+
+    // cout << "Sqauare root of the n is : " << sqrt(n) << endl;
+
+    // int a, b, c;
+    // cin >> a >> b >> c;
+
+    // Check for Determetestic Value
+    // float d = b * b - 4 * a * c;
+
+    // if (d < 0)
+    // {
+    //     cout << -1 << endl;
+    // }
+
+    // else
+    // {
+    //     int root1 = (-b + sqrt(d)) / (2 * a);
+    //     int root2 = (-b - sqrt(d)) / (2 * a);
+
+    //     cout << root1 << " " << root2 << endl;
+    // }
+
+    // int a = 10;
+
+    // cout << sizeof(a) << endl;
+
+    // double b = a;
+    // cout << b << endl;
+    // cout << sizeof(b) << endl;
+
+    // double value = 1100.55566;
+
+    // int a = value;
+    // cout<<
+
+    int a = 19;
+    int b = 2;
+
+    float ans = (a * 1.0) / b;
+    cout << ans << endl;
+
+    return 0;
+}
---
+++ 18-sept.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // int a = 10;
+
+    // int *ptr = &a;
+
+    // int **nextPtr = &ptr;
+
+    // cout << "The value present at the address of ptr is: " << *ptr << endl;
+    // cout << "The value present at the address of nextPtr is: " << **nextPtr << endl;
+    //     The address stored in ptr is: 0x61ff08
+    // The address stored in nextPtr is: 0x61ff04
+
+    // double a = 10.5;
+    // double *ptr = &a;
+
+    // cout << "The address where a is stored is: " << &a << endl;
+
+    // cout << "The address stored in ptr is: " << *nextPtr << endl;
+
+    // int a =100;
+    // int b= 200;
+
+    // int*aPtr = &a;
+    // int*bPtr = &b;
+
+    // int **nextPtr = &aPtr;
+
+    // int c = **nextPtr + *bPtr;
+    // cout << "The sum of a and b is: " << c << endl;
+
+    int *ptr = NULL;
+    cout << ptr << endl;
+    cout << *ptr << endl;
+    return 0;
+}
